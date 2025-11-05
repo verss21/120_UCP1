f2fc39874f2754c5310bdbd9256ab45fa5cad5d0
verss21 <arjunafrizal01@gmail.com>
2025-11-05 11:34:19 +0700

    menambahkan post

--- index.js
+++ index.js
@@ -23,35 +23,3 @@ app.post('/hotel', async (req, res) => {
     }
 });
 
-app.get('/hotel', async (req, res) => {
-    try {
-        const hotel = await db.Hotel.findAll();
-        res.send(hotel);
-    } catch (error) {
-        res.send({message: error.message});
-    }
-});
-
-app.put('/hotel/:id', async (req, res) => {
-    try {
-        const Hotel = await db.Hotel.findByPk(req.params.id);
-        if (!Hotel) return res.status(404).send({message: 'Hotel not found'});
-        
-        await Hotel.update(req.body);
-        res.send({message : "Hotel berhasil di update", Hotel});
-    } catch (error) {
-        res.send({message: error.message});
-    }
-});
-
-app.delete('/hotel/:id', async (req, res) => {
-    try {
-        const Hotel = await db.Hotel.findByPk(req.params.id);
-        if (!Hotel) return res.status(404).send({message: 'Hotel not found'});
-
-        await Hotel.destroy();
-        res.send({message : "Hotel berhasil dihapus", Hotel});
-    } catch (error) {
-        res.status(500).send({message: error.message});
-    }
-});
